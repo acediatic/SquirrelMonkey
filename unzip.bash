5eada764d73a0e5af99aeb36cd77d074f93ad08b
Adam Sinclair <hello@adamsinclair.kiwi>
2021-09-09 16:13:31 +1200

    init

---
+++ unzip.bash
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+read -p "Enter directory (. for current): " path
+
+for zip in $path/*.zip
+do
+  dirname=`echo $zip | sed 's/\.zip$//'`
+  if mkdir "$dirname"
+  then
+    if cd "$dirname"
+    then
+      unzip ../"$zip"
+      cd ..
+      rm -f "$zip" # Uncomment to delete the original zip file
+    else
+      echo "Could not unpack $zip - cd failed"
+    fi
+  else
+    echo "Could not unpack $zip - mkdir failed"
+  fi
+done
